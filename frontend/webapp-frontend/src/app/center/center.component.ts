372c221c2991aefa32df8530b60cedca77a4fada
demetrio1234 <dem.iaria@gmail.com>
2023-07-28 07:46:22 +0200

    EventEmitter through console and p tag works

--- frontend/webapp-frontend/src/app/center/center.component.ts
+++ frontend/webapp-frontend/src/app/center/center.component.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { PostElementComponent } from '../post-element/post-element.component';
 
 @Component({
   selector: 'app-center',
   templateUrl: './center.component.html',
   styleUrls: ['./center.component.css']
 })
-export class CenterComponent {
+export class CenterComponent implements OnInit, AfterViewInit {
 
   center: string = 'center';
+
+  @ViewChild(PostElementComponent) postElementComponent!: PostElementComponent;
+
+  variableOfCenter: string = '';
+
+  recivedOutputFromChild:string ='';
+
+  ngAfterViewInit(): void {
+
+    console.log(this.postElementComponent.messageFromPostElement);
+
+    this.center = this.postElementComponent.messageFromPostElement;
+
+  }
+  ngOnInit(): void {
+
+  }
+
+  reciveMessage($event: string) {
+    console.log($event);
+    this.recivedOutputFromChild = $event;
+  }
 
 }
